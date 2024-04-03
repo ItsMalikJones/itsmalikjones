d9367dfb21c00113341c6a034d6af14ad8094835
Malik Alleyne-Jones <malik@birdboar.co>
2024-04-03 00:17:08 -0400

    fathom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+        <!-- Fathom - beautiful, simple website analytics -->
+        <script src="https://cdn.usefathom.com/script.js" data-site="JKQESFDF" defer></script>
+        <!-- / Fathom -->
+
         @vite('resources/css/app.css')
     </head>
     <body x-data="{selectedProject: null, colors: { bgColor: 'bg-commonwealth-navy', textColor: 'text-white', accentColor: 'aqua'}, sounds: {}}" x-init="() => {
