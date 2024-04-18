3441e52737427c60f54ae951f53990d38bbb8397
Malik Alleyne-Jones <malik@birdboar.co>
2024-04-18 02:20:32 -0400

    fcps punch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Laravel\Dusk\Browser;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
+
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::post('/fcps/punch', function () {
+    $employeeNumber = request('employee_number');
+    $browser = new HttpBrowser(HttpClient::create());
+    $browser->request('GET', 'https://fayettecountysdky.executime.com/ExecuTime/Login.action?bmark=Index.action&kioskLogin=false');
+    $browser->submitForm('Sign In', [
+        'username' => $employeeNumber,
+        'password' => $employeeNumber,
+        'fastClock' => 1,
+    ]);
+    return $browser->getResponse();
+})->name('fcps.punch');
