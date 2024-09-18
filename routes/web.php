4af6d4461807951ae40477682b0d92968a5462d9
Malik Alleyne-Jones <malikjones02@gmail.com>
2024-09-17 21:54:56 -0400

    elb

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,3 @@ use Symfony\Component\HttpClient\HttpClient;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-
-Route::post('/fcps/punch', function () {
-    $employeeNumber = request('employee_number');
-    $browser = new HttpBrowser(HttpClient::create());
-    $browser->request('GET', 'https://fayettecountysdky.executime.com/ExecuTime/Login.action?bmark=Index.action&kioskLogin=false');
-    $browser->submitForm('Sign In', [
-        'username' => $employeeNumber,
-        'password' => $employeeNumber,
-    ]);
-
-    try {
-        $browser->clickLink('Clock in');
-    } catch (\Throwable $th) {
-        $browser->clickLink('Clock out');
-    }
-    return $browser->getResponse();
-})->name('fcps.punch');
