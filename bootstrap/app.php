83895d377fc8938170446866e83dc6a3b1a22584
Malik Alleyne-Jones <malik@kirschbaumdevelopment.com>
2024-09-19 15:36:53 -0400

    revert middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            at: '*',
-            headers: Request::HEADER_X_FORWARDED_AWS_ELB
-        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
